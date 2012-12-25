744af985eb787938720e58bfbcd2de24fee5c936
Durran Jordan <durran@gmail.com>
2012-12-24 19:16:53 -0500

    Allow use of summary attribute macros

--- lib/darstellung/macros.rb
+++ lib/darstellung/macros.rb
@@ -47,8 +47,7 @@ module Darstellung
     #
     # @since 0.0.0
     def detail(name, options = {}, &block)
-      normalized = name.to_sym
-      detail_attributes[normalized] = Attribute.new(normalized, options, &block)
+      create_attribute(name, detail_attributes, options, &block)
     end
 
     # Get all the attributes that are used in the detail representation.
@@ -65,6 +64,72 @@ module Darstellung
     # @since 0.0.0
     def detail_attributes
       @detail_attributes ||= {}
+    end
+
+    # Defines an attribute to be displayed in the summary representation of the
+    # resource.
+    #
+    # @example Display the name field in the summary display.
+    #   class UserResource
+    #     include Darstellung::Representable
+    #     summary :name
+    #   end
+    #
+    # @example Display the name field only from version 1.0.0
+    #   class UserResource
+    #     include Darstellung::Representable
+    #     summary :name, from: "1.0.0"
+    #   end
+    #
+    # @example Display the name field only from version 1.0.0 - 1.1.0
+    #   class UserResource
+    #     include Darstellung::Representable
+    #     summary :name, from: "1.0.0", to: "1.1.0"
+    #   end
+    #
+    # @example Display the name field as a custom representation.
+    #   class UserResource
+    #     include Darstellung::Representable
+    #
+    #     summary :name do |user|
+    #       user.full_name
+    #     end
+    #   end
+    #
+    # @param [ Symbol ] name The name of the attribute.
+    # @param [ Hash ] options The attribute options.
+    #
+    # @option options [ String ] :from The version the field is available from.
+    # @option options [ String ] :to The version the field is available to.
+    #
+    # @return [ Attribute ] The attribute object for the field.
+    #
+    # @since 0.0.0
+    def summary(name, options = {}, &block)
+      create_attribute(name, summary_attributes, options, &block)
+    end
+
+    # Get all the attributes that are used in the summary representation.
+    #
+    # @example Get all the summary attributes fields.
+    #   class UserResource
+    #     include Darstellung::Representable
+    #   end
+    #
+    #   UserResource.summary_attributes
+    #
+    # @return [ Hash ] The name/attribute pairs.
+    #
+    # @since 0.0.0
+    def summary_attributes
+      @summary_attributes ||= {}
+    end
+
+    private
+
+    def create_attribute(name, attributes, options = {}, &block)
+      normalized = name.to_sym
+      attributes[normalized] = Attribute.new(normalized, options, &block)
     end
   end
 end
--- spec/darstellung/macros_spec.rb
+++ spec/darstellung/macros_spec.rb
@@ -96,4 +96,99 @@ describe Darstellung::Macros do
       end
     end
   end
+
+  describe "#summary" do
+
+    before(:all) do
+      class User
+        attr_reader :name
+        def initialize(name)
+          @name = name
+        end
+        def long_name
+          "#{name}+"
+        end
+      end
+    end
+
+    after(:all) do
+      Object.__send__(:remove_const, :User)
+    end
+
+    context "when providing only an attribute name" do
+
+      before(:all) do
+        class UserResource
+          include Darstellung::Representable
+          summary :name
+        end
+      end
+
+      after(:all) do
+        Object.__send__(:remove_const, :UserResource)
+      end
+
+      let(:field) do
+        UserResource.summary_attributes[:name]
+      end
+
+      it "creates a summary attribute field" do
+        expect(field).to be_a(Darstellung::Attribute)
+      end
+
+      it "sets the name of the field" do
+        expect(field.name).to eq(:name)
+      end
+    end
+
+    context "when providing a version" do
+
+      before(:all) do
+        class UserResource
+          include Darstellung::Representable
+          summary :name, from: "1.0.0"
+        end
+      end
+
+      after(:all) do
+        Object.__send__(:remove_const, :UserResource)
+      end
+
+      let(:field) do
+        UserResource.summary_attributes[:name]
+      end
+
+      it "allows the field to be displayable by version" do
+        expect(field).to be_displayable("1.0.1")
+      end
+    end
+
+    context "when providing a block" do
+
+      before(:all) do
+        class UserResource
+          include Darstellung::Representable
+          summary :name do |user|
+            user.long_name
+          end
+        end
+      end
+
+      after(:all) do
+        Object.__send__(:remove_const, :UserResource)
+      end
+
+      let(:user) do
+        User.new("photek")
+      end
+
+      let(:field) do
+        UserResource.summary_attributes[:name]
+      end
+
+      it "allows using the block to get the field value" do
+        expect(field.value(user)).to eq("photek+")
+      end
+    end
+  end
 end
